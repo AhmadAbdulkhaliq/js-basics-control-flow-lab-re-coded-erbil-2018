ac66378641fb795ab6ab5a0f7193c5a9ea5c7778
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-26 19:48:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride)
+{
+  if(ride<=400)
+  {
+   return 'This one is on me!';
+  }
+  
+  else if(ride>2000 && ride<2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else{
+    return 'No can do.';
+  }
+}
+  
+function ternaryCheckCity(city){
+  
+  if(city==='NYC'){
+    return 'Ok, sounds good.';
+  }
+  else{
+    return 'No go.'
+  }
+
+}
